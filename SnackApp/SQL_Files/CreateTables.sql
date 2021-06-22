4ae0c1884387a4c331e305e95399179f23565cc1
Intactomundo <nicola.allenspach@bluewin.ch>
2021-06-22 17:42:47 +0200

    Setup repeatable database and table files

---
+++ SnackApp/SQL_Files/CreateTables.sql
@@ -0,0 +1,39 @@
+﻿-- ======================
+-- Create the different Tables
+-- ======================
+
+USE master
+GO
+
+-- Create Table users
+
+CREATE TABLE users(
+	userID int IDENTITY(1,1) PRIMARY KEY,
+	user_name varchar(255),
+	password varchar(255),
+	salt int,
+	verified bit,
+	is_admin bit
+);
+GO
+
+-- Create Table Items
+
+CREATE TABLE items(
+	itemID int IDENTITY(1,1) PRIMARY KEY,
+	item_name varchar(255),
+);
+GO
+
+-- Create Table user_items
+
+CREATE TABLE user_items(
+	user_itemsID int IDENTITY(1,1) PRIMARY KEY,
+	fk_userID int NOT NULL,
+	fk_itemID int NOT NULL,
+	month varchar(255),
+	year int,
+	FOREIGN KEY (fk_userID) REFERENCES users(userID),
+	FOREIGN KEY (fk_itemID) REFERENCES items(itemID)
+);
+GO
